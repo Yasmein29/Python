74481cd920ff77ad8b4a5bd1b8b0fbf54195ead8
Yasmein29 <nooryasmein@gmail.com>
2026-01-19 20:37:28 +0800

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,14 @@
+a = 3
+b = 5.4
+c = True
+d = "Hello!"
+
+print("Data type of a:", type(a))  # Output: <class 'int'>
+print("Data type of b:", type(b))  # Output: <class 'float'>
+print("Data type of c:", type(c))  # Output: <class 'bool'>
+print("Data type of d:", type(d))  # Output: <class 'str'>
+print("after type casting")
+a = float(a)
+b = int(b)
+print("Data type of a:", type(a))  # Output: <class 'int'>
+print("Data type of b:", type(b))  # Output: <class 'float'>
