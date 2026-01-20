f89636a3e304b74cf9a186b93eafe7cb2e496188
Yasmein29 <nooryasmein@gmail.com>
2026-01-20 20:43:55 +0800

    Add files via upload

---
+++ date&time.py
@@ -0,0 +1,10 @@
+print("Date, Time and Calendar")
+
+import datetime
+
+current_time = datetime.datetime.now()
+print("Current date and time:", current_time)
+
+import calendar
+print("\Calendar for 2026:")
+print(calendar.calendar(1967))
