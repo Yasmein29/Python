35dfff73412afd587b9da6a41dfc6f5a78caf27c
Yasmein29 <nooryasmein@gmail.com>
2026-01-27 19:37:53 +0800

    Add files via upload

---
+++ SwapnumHW.py
@@ -0,0 +1,20 @@
+x = 5
+y = 9
+z = 1
+
+# Swap the values of x, y, and z such that its in ascending order
+
+num = x
+num2 = y
+num3 = z
+x = z 
+y = num
+z = num2
+
+print("The value of x before swapping: ", num)
+print("The value of y before swapping: ", num2)
+print("The value of z before swapping: ", num3)
+
+print("The value of x after swapping: ", x)
+print("The value of y after swapping: ", y)
+print("The value of z after swapping: ", z)
