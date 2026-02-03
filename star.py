2fb43ec4edc594d7ee171689bd64a56387dd745a
Yasmein29 <nooryasmein@gmail.com>
2026-02-03 20:52:59 +0800

    Add files via upload

--- star.py
+++ star.py
@@ -1,6 +1,23 @@
-print("Printing a star pattern")
-n = int(input("Enter the number of rows you want to print: "))
-for i in range(1, n+1):
-    for j in range(i):
-        print("*", end=" ")
-    print()
+import turtle
+turtle.screen.bgcolor("black")
+board = turtle.Turtle()
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+
+board.penup()
+board.right(50)
+board.forward(50)
+board.pendown()
+
+board.right(90)
+board.forward(100)
+board.right(120)
+board.forward(100)
+board.right(120)
+board.forward(100)
+turtle.done()
