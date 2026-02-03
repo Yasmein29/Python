8a2c419e59c29d7731d9f187ad9eca219099ccd1
Yasmein29 <nooryasmein@gmail.com>
2026-02-03 20:44:02 +0800

    Add files via upload

---
+++ turtle.py
@@ -0,0 +1,10 @@
+import turtle
+sc = turtle.Screen()
+sc.setup(400, 300)
+turtle.title("Welcomme to Turtle")
+board = turtle.Turtle()
+
+for i in range(4):
+    board.forward(100)
+    board.right(90) 
+    i += 1
